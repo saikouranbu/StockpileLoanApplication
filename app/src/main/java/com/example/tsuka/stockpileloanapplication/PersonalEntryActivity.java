6f38bdfdeb90b3fd472254c72f56221ab9b81694
tsuka <saikouranbu@yahoo.co.jp>
2016-12-15 15:44:00 +0900

    文字色変更

---
+++ app/src/main/java/com/example/tsuka/stockpileloanapplication/PersonalEntryActivity.java
@@ -0,0 +1,13 @@
+package com.example.tsuka.stockpileloanapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonalEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_entry);
+    }
+}
