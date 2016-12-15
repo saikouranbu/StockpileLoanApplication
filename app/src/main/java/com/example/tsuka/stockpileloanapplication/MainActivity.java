e18a52c2f2ab0563b98cc94ae21af004d0f69ec5
tsuka <saikouranbu@yahoo.co.jp>
2016-12-15 15:26:43 +0900

    パーソナルデータ登録画面レイアウト作成 連絡先まで

--- app/src/main/java/com/example/tsuka/stockpileloanapplication/MainActivity.java
+++ app/src/main/java/com/example/tsuka/stockpileloanapplication/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.tsuka.stockpileloanapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION = 1; // 識別用:位置情報の許可
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // パーソナルデータ登録ボタンを押下時の挙動
+    public void onPersonalEntryClick(View v) {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // 位置情報の権限がある場合
+            // パーソナルデータ登録アクティビティに遷移
+            Intent intent = new Intent(this.getBaseContext(), PersonalEntryActivity.class);
+            startActivity(intent);
+        } else {
+            // 位置情報の権限がない場合
+            // 許可を求めるダイアログを表示
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION);
+        }
+    }
+
+    // パーミッションの許可を求めるダイアログ選択時に呼び出される
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        // 位置情報の権限を求める
+        if (requestCode == REQUEST_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 許可された場合
+                // パーソナルデータ登録アクティビティに遷移
+                Intent intent = new Intent(this.getBaseContext(), PersonalEntryActivity.class);
+                startActivity(intent);
+            } else {
+                // 許可されなかった場合
+                Toast.makeText(this, "位置情報の許可を出さない限りアプリを利用できません", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
